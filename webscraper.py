bdfca6f00795fe5d5c98a9713ac8b2e965e58c49
Alyssa Hammonds <81663980+senpailys@users.noreply.github.com>
2022-10-01 14:05:51 -0500

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -4,22 +4,19 @@ import pandas as pd
 import requests
 from urllib.request import urlopen
 
-
-
 #driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
-html = urlopen("https://www.arlington.org/events/")
+html = urlopen("https://www.arlington.org/events/?view=list&sort=date")
 bsObj = BeautifulSoup(html, "html.parser")
 
 eventNames = bsObj.findAll("span", {"class":"title truncate"})
 for name in eventNames:
     print(name.get_text())
 
-dates = bsObj.findAll("span", {"class":"month"})
+dates = bsObj.findAll("span", {"class":"mini-date-container"})
 for date in dates:
     print(date.get_text())
 
 eventLocations = bsObj.findAll("span", {"class":"locations truncate"})
 for location in eventLocations:
     print(location.get_text())
-    
     
