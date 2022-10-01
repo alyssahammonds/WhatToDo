f4640d0eff784fb8e07dbf239e3b4d1bd042597c
Alyssa Hammonds <81663980+senpailys@users.noreply.github.com>
2022-10-01 16:49:35 -0500

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,22 +1,26 @@
 from selenium import webdriver
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 from urllib.request import urlopen
 
-#driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
-html = urlopen("https://www.arlington.org/events/?view=list&sort=date")
-bsObj = BeautifulSoup(html, "html.parser")
 
-eventNames = bsObj.findAll("span", {"class":"title truncate"})
-for name in eventNames:
-    print(name.get_text())
+# driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+def myfunc():
+    url = "https://www.arlington.org/events/?view=list&sort=date"
+    results = requests.get(url)
+    doc = BeautifulSoup(results.text, "html.parser")
+    #print(doc.prettify())
+    eventNames = doc.find_all("a")
+    for name in eventNames:
+        print(name.find_next_sibling())
+    dates = doc.findAll("span", {"class": "mini-date-container"})
+    for date in dates:
+        print(date.get_text())
 
-dates = bsObj.findAll("span", {"class":"mini-date-container"})
-for date in dates:
-    print(date.get_text())
+    eventLocations = doc.findAll("span", {"class": "locations truncate"})
+    for location in eventLocations:
+        print(location.get_text())
 
-eventLocations = bsObj.findAll("span", {"class":"locations truncate"})
-for location in eventLocations:
-    print(location.get_text())
-    
+
+myfunc()
