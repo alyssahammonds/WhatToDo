f172076b9e0ce5f4dc0fbe7f88777ef16160e678
Alyssa Hammonds <81663980+senpailys@users.noreply.github.com>
2022-10-01 13:51:05 -0500

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -4,23 +4,22 @@ import pandas as pd
 import requests
 from urllib.request import urlopen
 
-range_miles = [] #store range from user
-prices = [] #store price of the product
-ratings = [] #store rating of the product
 
 
 #driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
-html = urlopen("https://www.michigan.org/events?page=0")
+html = urlopen("https://www.arlington.org/events/")
 bsObj = BeautifulSoup(html, "html.parser")
 
-eventNames = bsObj.findAll("span", {"class":"field field--name-field-display-title field--type-string field--label-hidden field__item"})
+eventNames = bsObj.findAll("span", {"class":"title truncate"})
 for name in eventNames:
     print(name.get_text())
 
-dates = bsObj.findAll("span", {"class":"event-dates"})
+dates = bsObj.findAll("span", {"class":"month"})
 for date in dates:
     print(date.get_text())
 
-eventLocations = bsObj.findAll("span", {"class":"field field--name-field-cities field--type-entity-reference field--label-hidden field__items"})
+eventLocations = bsObj.findAll("span", {"class":"locations truncate"})
 for location in eventLocations:
     print(location.get_text())
+    
+    
