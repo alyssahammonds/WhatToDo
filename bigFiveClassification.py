300c679f62c29651923b42ed98b468eb1f69842f
Alyssa Hammonds <81663980+senpailys@users.noreply.github.com>
2022-10-01 21:34:30 -0500

    Update bigFiveClassification.py

--- bigFiveClassification.py
+++ bigFiveClassification.py
@@ -1,4 +1,3 @@
-
 classificationWords = {
     "Openness": ["Adventurous", "Creative", "Artistic", "Curious", "Abstract"],
     "Conscientiousness": ["Self-Disciplined", "Organized", "Plan-Driven", "Aware", "Detail Focused"],
@@ -6,12 +5,35 @@ classificationWords = {
     "Agreeableness": ["Empathetic", "Trusting", "Honest", "Helping", "Put others first"],
     "Neuroticism": ["Stress Easy", "Moody", "Worrisome", "Emotionally Unstable", "Irritable"]
 }
-    
 
+classifictions = ["Openness", "Conscientiousness", "Extraversion", "Agreeableness", "Neuroticism"]
+
+attributes = ["Adventurous", "Plan-Driven", "Detail Focused", "Honest", "Helping", "Creative", "Sociable", "Aware", "Curious"]
 
 #This function classifies the scores for each big 5 attribute by 
 def classificationScore(attributeChosen):
     #each element of the scores represents a classification score
     #openness being 0, conscientiosness 1, Extraversion 2, ... matching the structure that the dict was made
-    score = []
+    score = [0, 0, 0, 0, 0]
     
+    counter = 0
+    for classification in classificationWords:
+        for word in classificationWords[classification]:
+            for attribute in attributeChosen:
+                if(word == attribute):
+                    score[counter] = score[counter] + 1
+        counter = counter + 1
+    
+    if score.index(max(score)) == 0:
+        return "Openness"
+    elif score.index(max(score)) == 1:
+        return "Conscientiousness"
+    elif score.index(max(score)) == 2:
+        return "Extravertion"
+    elif score.index(max(score)) == 3:
+        return "Agreeableness"
+    
+    return "Neuroticism"
+        
+
+print(classificationScore(attributes))  
