5ed433d7589389b1374f64887359a9f971f71511
chanuthi <36820617+chanuthi@users.noreply.github.com>
2022-10-02 06:40:44 -0500

    Update grid.py
    hi lys <3

--- grid.py
+++ grid.py
@@ -1,25 +1,41 @@
 import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
 
 root = tk.Tk()
-apps = []
+list = []
 root.title("grid() method")
-root.geometry("390x844")
+root.geometry("390x844") 
 #da frame on topa canvas
 frame = tk.Frame(root, bg="white")
 frame.place(relwidth=390,relheight=844)
+
+#the text on buttons
 Word =[['Adventurous', 'Creative', 'Artistic', "Curious", "Abstract"],
     ["Self-Disciplined", "Organized", "Plan-Driven", "Aware", "Detail Focused"],
     ["Energetic", "Center of Attention", "Assertive", "Impulsive", "Sociable"],
     ["Empathetic", "Trusting", "Honest", "Helping", "Put others first"],
     ["Stress Easy", "Moody", "Worrisome", "Emotionally Unstable", "Irritable"]]
 
+def getchar(w,a,Word):
+    list.append(Word[w][a])
+
+
+
+buttonList = []
+
+#making 1 button per element in Word     
 for w in range(len(Word)):
+    #print(w)
     for a in range(len(Word)):
-        button1 = tk.Button(root, text=Word[w][a],height =5,width=5,bd=6,padx=18.50,pady=18.50)
+        #print(a)
+        button1 = tk.Button(root,height =4,width=5,bd=6,padx=18.50,pady=18.50)
         button1.grid(row=w,column=a)
-
-
-
+        buttonList.append(button1)
+        button1.config(text=Word[w][a], justify =LEFT) #trying to put the word on the button into list[]
 root.mainloop()
+
+for x in range(len(buttonList)):
+    print(type(buttonList[x]))
 
