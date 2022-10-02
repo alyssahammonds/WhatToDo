25110f4d7f97bec033ecfa3a0b0e773b613d6140
Alyssa Hammonds <81663980+senpailys@users.noreply.github.com>
2022-10-01 19:10:25 -0500

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,11 +1,11 @@
-from selenium import webdriver
+
 from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 from urllib.request import urlopen
 
 
-def myfunc():
+def socialevents():
     url = "https://www.eventbrite.com/d/tx--arlington/events/"
     results = requests.get(url)
     doc = BeautifulSoup(results.text, "html.parser")
@@ -13,17 +13,14 @@ def myfunc():
     eventnamesarr = []
     eventNames = doc.find_all('div', class_= 'eds-event-card__formatted-name--is-clamped eds-event-card__formatted-name--is-clamped-three eds-text-weight--heavy')
     for name in eventNames:
-        eventnamesarr.append(name.get_text())
+        eventnamesarr.append(name.get_text()) #create an array of event names
 
     datesarrray = []
     dates = doc.find_all('div', class_ = 'eds-event-card-content__sub-title eds-text-color--primary-brand eds-l-pad-bot-1 eds-l-pad-top-2 eds-text-weight--heavy eds-text-bm')
     for date in dates:
-        datesarrray.append(date.get_text())
+        datesarrray.append(date.get_text()) #create an array of event dates
 
     arrayLocations = []
     eventLocation = doc.find_all('div', class_ = 'eds-event-card-content__sub eds-text-bm eds-text-color--ui-600 eds-l-mar-top-1')
     for location in eventLocation:
-        arrayLocations.append(location.get_text())
-
-
-myfunc()
+        arrayLocations.append(location.get_text()) #create an array of event locations
