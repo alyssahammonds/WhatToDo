830c8c2e832c66ea7257055943e71b840fe5aa94
Lys <alyssahammonds5453@gmail.com>
2022-10-03 16:51:29 -0500

    Revert "Update webscraper.py"
    This reverts commit 4dfccf6c72da8b46fca37594f0b9f3931f211d4d.

--- webscraper.py
+++ webscraper.py
@@ -6,7 +6,6 @@ from urllib.request import urlopen
 # eventbrite.com lets hosts post events to advertise
 # This program will categorize users of our app and return the
 # name and date of events near them that fit their personality.
-# This is the main chunk of our HACKUTA 2022 submission
 
 def allevents():
     #default
